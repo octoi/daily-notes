e3b21de9289763addfa91a8e8bc14e834a37bfda
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-26 07:17:51 +0530

    added icon btn

--- app/lib/widgets/topbar.dart
+++ app/lib/widgets/topbar.dart
@@ -1,3 +1,4 @@
+import 'package:app/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 class Topbar extends StatelessWidget {
@@ -6,8 +7,11 @@ class Topbar extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Text(
                 "n",
@@ -25,6 +29,18 @@ class Topbar extends StatelessWidget {
               ),
             ],
           ),
+          Container(
+            margin: EdgeInsets.only(top: 5.0),
+            child: IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {},
+              color: Colors.white38,
+              iconSize: 20.0,
+              splashColor: appSecondaryColor,
+              highlightColor: appSecondaryColor,
+              splashRadius: 20.0,
+            ),
+          )
         ],
       ),
     );
