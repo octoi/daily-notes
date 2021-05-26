e968fd1e05592d6b8d81224b3d1b21aa97e40348
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-26 07:07:47 +0530

    added some tuffs

--- app/lib/screens/app_screen.dart
+++ app/lib/screens/app_screen.dart
@@ -1,8 +1,20 @@
+import 'package:app/utils/constants.dart';
+import 'package:app/widgets/topbar.dart';
 import 'package:flutter/material.dart';
 
 class AppScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Topbar(),
+          ],
+        ),
+      ),
+    );
   }
 }
