b6466d2866c7403808af0e15829b4511ab9a618b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-28 06:41:28 +0530

    cleaned

--- app/lib/screens/app_screen.dart
+++ app/lib/screens/app_screen.dart
@@ -5,20 +5,20 @@ import 'package:flutter/material.dart';
 class AppScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: appPrimaryColor,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: appPrimaryAccentColor,
-        child: Icon(Icons.note_add_outlined),
-        onPressed: () {},
-      ),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            Topbar(),
-          ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: appPrimaryColor,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: appPrimaryAccentColor,
+          child: Icon(Icons.note_add_outlined),
+          onPressed: () {},
         ),
+        appBar: Topbar(),
+        body: TabBarView(children: [
+          Text("Hello World"),
+          Text("Hello World"),
+        ]),
       ),
     );
   }
