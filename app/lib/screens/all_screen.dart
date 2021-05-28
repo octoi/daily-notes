8be6157c3574b3e34bceb6ef9658dd306820e04d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-28 14:57:21 +0530

    all screen widgets and stuffs

--- app/lib/screens/all_screen.dart
+++ app/lib/screens/all_screen.dart
@@ -4,33 +4,53 @@ import 'package:flutter/material.dart';
 class AllScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return GridView.count(
       physics: BouncingScrollPhysics(),
+      crossAxisCount: 2,
+      children: [
+        Note(),
+        Note(),
+        Note(),
+        Note(),
+        Note(),
+      ],
+    );
+  }
+}
+
+class Note extends StatelessWidget {
+  const Note({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      margin: EdgeInsets.all(5.0),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: appSecondaryColor,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            width: double.infinity,
-            height: 200.0,
-            color: appSecondaryColor,
-            margin: EdgeInsets.all(10.0),
+          Text(
+            "Lorem Ipsum",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-          Container(
-            width: double.infinity,
-            height: 200.0,
-            color: appSecondaryColor,
-            margin: EdgeInsets.all(10.0),
-          ),
-          Container(
-            width: double.infinity,
-            height: 200.0,
-            color: appSecondaryColor,
-            margin: EdgeInsets.all(10.0),
-          ),
-          Container(
-            width: double.infinity,
-            height: 200.0,
-            color: appSecondaryColor,
-            margin: EdgeInsets.all(10.0),
+          SizedBox(height: 10.0),
+          Expanded(
+            child: Text(
+              "Flutter is awesome because it is just awesome, Flutter is awesome because it is just awesome, Flutter is awesome because it is just awesome, Flutter is awesome because it is just awesome, Flutter is awesome because it is just awesome",
+              style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w400),
+              overflow: TextOverflow.fade,
+            ),
           ),
         ],
       ),
