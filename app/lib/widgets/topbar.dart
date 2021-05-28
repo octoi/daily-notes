f4ab9ae5438a95b425cbce22ec831b0efec44157
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-28 06:39:52 +0530

    changed container to appbar and added tabs

--- app/lib/widgets/topbar.dart
+++ app/lib/widgets/topbar.dart
@@ -1,36 +1,40 @@
 import 'package:app/utils/constants.dart';
 import 'package:flutter/material.dart';
 
-class Topbar extends StatelessWidget {
+class Topbar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 15.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return AppBar(
+      title: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "n",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 25.0,
-                ),
-              ),
-              Text(
-                "otes",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-            ],
+          Text(
+            "n",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25.0,
+            ),
+          ),
+          Text(
+            "otes",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
           ),
         ],
       ),
+      bottom: TabBar(
+        tabs: [
+          Tab(icon: Icon(Icons.directions_car)),
+          Tab(icon: Icon(Icons.directions_transit)),
+        ],
+      ),
+      backgroundColor: appPrimaryColor,
+      elevation: 0.0,
     );
   }
 }
