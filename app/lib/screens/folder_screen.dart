f9f8d48b334d37a4e90077fe79b791abcee4f7a5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-30 08:00:40 +0530

    extracted folder

--- app/lib/screens/folder_screen.dart
+++ app/lib/screens/folder_screen.dart
@@ -1,4 +1,4 @@
-import 'package:app/utils/constants.dart';
+import 'package:app/widgets/folder_display.dart';
 import 'package:flutter/material.dart';
 
 class FolderScreen extends StatelessWidget {
@@ -15,37 +15,9 @@ class FolderScreen extends StatelessWidget {
         Folder(),
         Folder(),
         Folder(),
+        Folder(),
+        Folder(),
       ],
-    );
-  }
-}
-
-class Folder extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: appSecondaryColor,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.folder_rounded,
-            size: 80.0,
-            color: appSecondaryAccentColor,
-          ),
-          Text(
-            "Folder Name",
-            style: TextStyle(
-              color: Colors.grey,
-            ),
-            overflow: TextOverflow.ellipsis,
-          )
-        ],
-      ),
     );
   }
 }
