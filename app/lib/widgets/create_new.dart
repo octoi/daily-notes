bd612d9e0b5b9c35833c6157d40f6f6194181edb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-30 08:58:04 +0530

    alert

--- app/lib/widgets/create_new.dart
+++ app/lib/widgets/create_new.dart
@@ -1,34 +1,63 @@
-import 'package:app/screens/folder_screen.dart';
+import 'package:app/utils/constants.dart';
 import 'package:flutter/material.dart';
 
-void createFolder(BuildContext context) {
-  showDialog(
+Future<void> _showMyDialog(context) async {
+  return showDialog<void>(
     context: context,
     builder: (BuildContext context) {
-      return Text(
-        "create folder",
-        style: TextStyle(color: Colors.white),
-      );
-    },
-  );
-}
-
-void createNote(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return Text(
-        "create note",
-        style: TextStyle(color: Colors.white),
+      return AlertDialog(
+        title: const Text(
+          'New document',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: appSecondaryColor,
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: const <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "document name",
+                  labelStyle: TextStyle(color: Colors.white30),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: appPrimaryAccentColor,
+                      width: 0.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: appPrimaryAccentColor,
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          RaisedButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("cancel"),
+            highlightColor: appSecondaryColor,
+            color: appPrimaryColor,
+            textColor: Colors.white,
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text("create"),
+            highlightColor: appSecondaryColor,
+            color: appPrimaryAccentColor,
+            textColor: Colors.white,
+          ),
+        ],
       );
     },
   );
 }
 
 void createThing(context, isFolderScreen) {
-  if (isFolderScreen) {
-    createFolder(context);
-  } else {
-    createNote(context);
-  }
+  _showMyDialog(context);
 }
