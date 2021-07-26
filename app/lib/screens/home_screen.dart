55da61ab88d6f219bfbe38864b7f60f45dca4de2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-26 12:52:48 +0530

    added button

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:app/utils/constants.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +7,35 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: appPrimaryColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Material(
+                color: appSecondaryColor,
+                borderRadius: BorderRadius.circular(10.0),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: appPrimaryColor,
+                  highlightColor: appPrimaryColor,
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric(vertical: 50.0),
+                    child: Icon(
+                      CupertinoIcons.add_circled,
+                      color: appPrimaryAccentColor,
+                      size: 50.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
