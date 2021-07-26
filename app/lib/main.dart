f5733674b3cb39ec9832f7ed8c71f8c11ba93313
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-26 15:24:54 +0530

    added theme

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -12,6 +12,7 @@ class DailyNotes extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: HomeScreen(),
     );
   }
