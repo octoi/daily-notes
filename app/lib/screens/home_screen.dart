0707a38db7161555d79260b23b8be025d73b499e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-26 13:12:19 +0530

    note

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -43,8 +43,47 @@ class HomeScreen extends StatelessWidget {
                 child: Text(
                   "Recent Notes",
                   style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
-                    fontSize: 18.0,
+                    color: appPrimaryTextColor,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10.0),
+                child: Material(
+                  color: appSecondaryColor,
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: InkWell(
+                    onTap: () {},
+                    splashColor: appPrimaryColor,
+                    highlightColor: appPrimaryColor,
+                    child: Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.all(15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Note Title",
+                            style: TextStyle(
+                              color: appPrimaryTextColor,
+                              fontSize: 18.0,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            "Officia eiusmod minim deserunt duis cupidatat consectetur sint veniam velit non officia esse occaecat. Esse ipsum sint aute cillum aute ea. Do minim elit voluptate proident dolore do quis est ad do quis. Mollit anim reprehenderit quis labore non et irure proident sunt. Aliqua velit irure officia ullamco enim elit consequat consequat do ullamco nisi dolore anim quis. Ipsum ex consequat id Lorem."
+                                    .substring(0, 200) +
+                                " ...",
+                            style: TextStyle(
+                              color: appPrimaryTextColor,
+                              fontSize: 15.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
