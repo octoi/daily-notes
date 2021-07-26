e770cadfe6c5fbdf17c974e2e5792c06bcf5e651
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-26 13:14:12 +0530

    ...

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:app/utils/constants.dart';
+import 'package:app/widgets/note.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -48,45 +49,11 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 10.0),
-                child: Material(
-                  color: appSecondaryColor,
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: InkWell(
-                    onTap: () {},
-                    splashColor: appPrimaryColor,
-                    highlightColor: appPrimaryColor,
-                    child: Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.all(15.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Note Title",
-                            style: TextStyle(
-                              color: appPrimaryTextColor,
-                              fontSize: 18.0,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          SizedBox(height: 10.0),
-                          Text(
-                            "Officia eiusmod minim deserunt duis cupidatat consectetur sint veniam velit non officia esse occaecat. Esse ipsum sint aute cillum aute ea. Do minim elit voluptate proident dolore do quis est ad do quis. Mollit anim reprehenderit quis labore non et irure proident sunt. Aliqua velit irure officia ullamco enim elit consequat consequat do ullamco nisi dolore anim quis. Ipsum ex consequat id Lorem."
-                                    .substring(0, 200) +
-                                " ...",
-                            style: TextStyle(
-                              color: appPrimaryTextColor,
-                              fontSize: 15.0,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              Note(),
+              Note(),
+              Note(),
+              Note(),
+              Note(),
             ],
           ),
         ),
