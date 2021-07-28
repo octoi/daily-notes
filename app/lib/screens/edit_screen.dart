2a5cd233238c1241d5f3e47f1d73409e4afef569
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-28 12:52:05 +0530

    ...

--- app/lib/screens/edit_screen.dart
+++ app/lib/screens/edit_screen.dart
@@ -3,9 +3,12 @@ import 'package:app/widgets/app_tab_bar_view.dart';
 import 'package:flutter/material.dart';
 
 class EditScreen extends StatelessWidget {
-  final String title;
+  final String fileName;
 
-  const EditScreen({Key? key, this.title = ''}) : super(key: key);
+  const EditScreen({
+    Key? key,
+    this.fileName = '',
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,7 @@ class EditScreen extends StatelessWidget {
           backgroundColor: appPrimaryColor,
           elevation: 0.0,
           title: Text(
-            title.trim() == '' ? 'Untitled' : title,
+            fileName.trim() == '' ? 'Untitled' : fileName,
             style: TextStyle(
               color: appWhite,
               fontWeight: FontWeight.normal,
